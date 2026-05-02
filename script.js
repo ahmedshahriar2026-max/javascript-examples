9556c8d03b94caae54874704c7fde644bc594a2c
ahmedalshahriyar <ahmedshahriar2026@gmail.com>
2026-05-02 10:55:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+//js
+// ex1
+function showText(){
+	document . getElementById("st") . innerHTML
+	="Hello world";
+}
+// ex2
+function showDate(){
+	document . getElementById("sd") . innerHTML
+	=Date();
+}
+// ex3
+function showDates(){
+	document . getElementById("sdate") . innerHTML
+	=Date();
+}
+// ex4
+function clickHere(){
+	document . getElementById("ch") . innerHTML
+	="Hello jarvis";
+}
+// ex5
+function clickMe(){
+	document . getElementById("cm") . innerHTML
+	="Hello Javascript!";
+}
+// ex6
+function tryIt() {
+  document.getElementById("ti") . innerHTML 
+  = "Paragraph changed.";
+}
+// ex7
+function bulbon(){
+	document.getElementById('img').src = "img/on.gif";
+}
+function bulboff(){
+	document.getElementById('img').src = "img/off.gif";
+}
+// ex8
+function showoutput(){
+	var number1=5;
+	var number2=8;
+	var result=number1+number2
+	document.write(result);
+	}
+
+// ex9
+	function printPage(){
+		window.print();
+	}
+
+
+
+
+
+
+
+
+
+
